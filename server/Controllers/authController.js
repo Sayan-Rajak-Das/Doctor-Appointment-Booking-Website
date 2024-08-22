4da022acdf55479bfd33bf919f75746412397dfd
Sayan-Rajak-Das <sayandas987123654@gmail.com>
2024-08-22 23:39:52 +0530

    Switched to new branch

---
+++ server/Controllers/authController.js
@@ -0,0 +1,19 @@
+
+export const register = async (req, res) => {
+
+    const {email, password, name , role, photo, gender} = req.body;
+
+    try {
+
+    } catch (err){
+
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+
+    } catch (err){
+
+    }
+};
